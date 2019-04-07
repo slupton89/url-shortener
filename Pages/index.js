0f97343c5beaf60315aa3ea3e00ee039382d2303
Shane Lupton <slupton89@gmail.com>
2019-04-06 23:54:10 -0600

    added logo and form elements

--- Pages/index.js
+++ Pages/index.js
@@ -2,10 +2,24 @@
 import ('./scss/index.scss')
 
 function Index() {
+  let urlInput = ''
 
   return (
     <div className="main">
-      <p>Hello Next.js!</p>
+      <img className="logo" src='../Static/logo.png' alt="logo" />
+      <img className="title" src='../Static/shrinky.png' alt="logo" />
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        console.log('Form submitted!', urlInput)
+      }}>
+      <input type="text" className="textField"
+        name="url"
+        placeholder="Enter a url to shrink"
+        defaultValue=''
+        onChange={(e) => {urlInput = e.target.value}}
+        required></input>
+      <button type="submit" className="submitBtn">Shrink!</button>
+      </form>
     </div>
   )
 }
